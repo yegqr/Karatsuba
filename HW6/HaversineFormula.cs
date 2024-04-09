ed733d1e8de19229b3f980a4f2f871c62b15bd3a
zuk <yisakov@kse.org.ua>
2024-04-09 18:12:30 +0300

    Initial commit

---
+++ HW6/HaversineFormula.cs
@@ -0,0 +1,31 @@
+﻿namespace HW6;
+
+public class HaversineFormula
+{
+    private const double EarthRadiusKm = 6371;
+
+    private static double DegreesToRadians(double degrees)
+    {
+        return degrees * Math.PI / 180.0;
+    }
+
+    public static double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+    {
+        lat1 = DegreesToRadians(lat1);
+        lon1 = DegreesToRadians(lon1);
+        lat2 = DegreesToRadians(lat2);
+        lon2 = DegreesToRadians(lon2);
+
+        var dLat = lat2 - lat1;
+        var dLon = lon2 - lon1;
+
+        var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(lat1) * Math.Cos(lat2) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        var distance = EarthRadiusKm * c;
+
+        return distance;
+    }
+}
---
+++ HW6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using HW6;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var sw = new Stopwatch();
+        sw.Start(); 
+        double lat = 0;
+        double lonh = 0;
+        double radius = 0;
+        
+        for (int i = 0; i < args.Length; i++)
+        {
+            var temp = args[i].Split("=");
+            
+            if (temp[0] == "--lat")
+            {
+                double.TryParse(temp[1], out lat);
+            }
+            else if (temp[0] == "--long")
+            {
+                double.TryParse(temp[1], out lonh);
+            }
+            else if (temp[0] == "--size")
+            {
+                double.TryParse(temp[1], out radius);
+            }
+        }
+        
+        foreach (var row in File.ReadAllLines(@"C:\\Users\\iskos\\RiderProjects\\HW6\\HW6\\ukraine_poi.csv"))
+        {
+            var splited = row.Split(";");
+            var distance = HaversineFormula.CalculateDistance(Convert.ToDouble(splited[0]),
+                Convert.ToDouble(splited[1]), lat, lonh);
+                                       ;
+            if ( distance <= radius)
+            {
+                Console.WriteLine(row);
+            }
+        }
+        
+        sw.Stop();
+        Console.WriteLine($"Elapsed time: {sw.Elapsed}");
+    }
+    
+}
